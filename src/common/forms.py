d917c9582b17b02a103bbbfdbbe3b2a8fc957276
Quest_254 <uvaqrec@gmail.com>
2019-05-18 10:24:21 +0300

    first social_app commit

---
+++ src/common/forms.py
@@ -0,0 +1,80 @@
+#! usr/bin/env python
+
+from flask_wtf import FlaskForm, validators
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import (DataRequired, Regexp, ValidationError, Email,
+                                Length, EqualTo)
+
+from src.common.models import User
+
+
+def name_exists(form, field):
+    if User.select().where(User.username == field.data).exists():
+        raise ValidationError('User with that name already exists.')
+
+
+def email_exists(form, field):
+    if User.select().where(User.email == field.data).exists():
+        raise ValidationError('User with that email already exists.')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(
+        'Username',
+        validators=[
+            DataRequired(),
+            Regexp(
+                r'^[a-zA-Z0-9_]+$',
+                message=("Username should be one word, letters, "
+                         "numbers, and underscores only.")
+            ),
+
+        ]
+    )
+    email = StringField(
+        'Email',
+        validators=[
+            DataRequired(),
+            Email(),
+
+        ]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[
+            DataRequired(),
+            Length(min=2),
+            EqualTo('password2', message='Passwords must match')
+        ]
+    )
+    password2 = PasswordField(
+        'Confirm Password',
+        validators=[
+            DataRequired()
+        ]
+    )
+
+
+class LoginForm(FlaskForm):
+    email = StringField(
+        'Email',
+        validators=[
+            DataRequired(),
+            Email()
+        ]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[
+            DataRequired()
+        ]
+    )
+
+
+class PostForm(FlaskForm):
+    content = TextAreaField(
+        "What's up?",
+        validators=[
+            DataRequired()
+        ]
+    )
